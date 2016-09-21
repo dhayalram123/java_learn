0e6b95526e808780926ca5b7094af2de6c5512d2
Dhayal Ram <dhayalram123@gmail.com>
2016-09-21 18:58:44 +0530

    My First Java Program

---
+++ src/com/dhayal/ram/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.dhayal.ram;
+
+/**
+ * @author dhayal.r
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Rocking world");
+	}
+
+}
